73f39ad9dc84e5fa0d0d144b1b2c260b0639dccb
romantrety <114093642+22057221@users.noreply.github.com>
2022-09-21 18:53:19 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,60 @@
+﻿//#include <iostream>
+//using namespace std;
+//int main()
+//{
+	//setlocale(LC_ALL, "RUSSIAN");
+	//Boolean1 Дано целое число A.Проверить истинность высказывания : «Число A является положительным».
+	//int a;
+	//cin >> a;
+	//bool res = a > 0;
+	//cout << boolalpha << res << endl;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+	//setlocale(LC_ALL, "RUSSIAN");
+	//Boolean2◦Дано целое число A.Проверить истинность высказывания : «Число A является нечетным»
+	//int a;
+	//cin >> a;
+	//bool res = a % 2 != 0;
+	//cout << boolalpha << res << endl;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+	//setlocale(LC_ALL, "RUSSIAN");
+	//Boolean3◦ Дано целое число A.Проверить истинность высказывания : «Число A является четным».
+	//int a;
+	//cin >> a;
+	//bool res = a % 2 == 0;
+	//cout << boolalpha << res << endl;
+//}
+
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+		//Boolean4◦Даны два целых числа : A, B.Проверить истинность высказывания :«Справедливы неравенства A > 2 и B ≤ 3».		//int a, b;
+	    //cin >> a >> b;
+		//bool res = 2 && b <= 3;
+		//cout << boolalpha << res << endl;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//;int main()
+//{
+		//Boolean4◦Даны два целых числа : A, B.Проверить истинность высказывания :«Справедливы неравенства A > 2 и B ≤ 3».		//int a, b;
+		//cin >> a >> b;
+		//bool res = a >= 0 || b <= 3;
+		//cout << boolalpha << res << endl;
+//}
